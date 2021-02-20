39d8b1f91744006b9355cb40ce5540da936ff3b0
PratikChakraborty10 <chakrabortypratik2000@gmail.com>
2021-02-20 08:07:55 +0530

    Initial Commit

---
+++ app/src/main/java/in/pratikchakraborty/todolist/Task.kt
@@ -0,0 +1,24 @@
+package `in`.pratikchakraborty.todolist
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+enum class SortColumn {
+    Title, Priority
+}
+
+enum class TaskStatus {
+    Open, Closed
+}
+
+enum class PriorityLevels {
+    Low, Medium, High
+}
+
+@Entity(tableName = "task")
+data class Task(@PrimaryKey(autoGenerate = true) val id: Long,
+                val title: String,
+                val detail: String,
+                @ColumnInfo(name = "priority") val priority: Int,
+                val status: Int)
